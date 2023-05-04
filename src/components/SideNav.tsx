77681d3c89da441d14275c75fbf3f3c84a27ef1c
cbolles <collinbolles@gmail.com>
2023-05-04 11:34:16 -0400

    Cleanup sidenav

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -1,53 +1,94 @@
-import React, { useContext } from 'react'
-import { Drawer, List, ListItem, ListItemButton } from '@mui/material'
-import { Link, useNavigate } from "react-router-dom";
+import { FC, useContext } from 'react'
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  Box,
+  ListItemIcon,
+  ListItemText
+} from '@mui/material'
+import { useNavigate } from "react-router-dom";
 import { AuthContext } from '../contexts/Auth';
 import logo from '../assets/nist-logo.png';
+import StarIcon from '@mui/icons-material/Star';
 
-export default function SideNav() {
+export const SideNav: FC = () => {
+  const drawerWidth = 256;
 
-    const { setIsAuthenticated, setToken } = useContext(AuthContext);
-    const navigate = useNavigate();
-    
-    const logout = () => {
-        localStorage.removeItem('token');
-        setToken("");
-        setIsAuthenticated(false);
-        navigate('/login');
-    }
+  const { setIsAuthenticated, setToken } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-    return (
-        <div>
-            <Drawer
-                variant="permanent"
-                anchor="left"
-                open={true}
-            >
-                <div>
-                    <img src={logo} alt="NIST Logo" style={{width: 150, height: 100}}/>
-                </div>
-                <List>
-                    <ListItem>
-                        <Link to={`dashboard`}>
-                            <ListItemButton>
-                                Dashboard
-                            </ListItemButton>
-                        </Link>
-                    </ListItem>
-                    <ListItem>
-                        <Link to={`access-manager`}>
-                            <ListItemButton>
-                                Permissions
-                            </ListItemButton>
-                        </Link>
-                    </ListItem>
-                    <ListItem>
-                        <ListItemButton onClick={logout}>
-                            Logout
-                        </ListItemButton>
-                    </ListItem>
-                </List>
-            </Drawer>
-        </div>
-    )
+  const logout = () => {
+    localStorage.removeItem('token');
+    setToken("");
+    setIsAuthenticated(false);
+    navigate('/login');
+  }
+
+  return (
+    <Drawer
+      variant='permanent'
+      sx={{
+        width: drawerWidth,
+        flexShrink: 0,
+        '& .MuiDrawer-paper': {
+          width: drawerWidth,
+          boxSizing: 'border-box',
+        },
+      }}
+      anchor='left'
+    >
+
+    <Box
+      component='img'
+      sx={{
+        height: 131,
+        width: drawerWidth
+      }}
+      alt='NIST Racer Logo'
+      src={logo}
+    />
+
+    <Box
+      sx={{
+        marginRight: '8px',
+        marginLeft: '8px',
+        marginBottom: '16px',
+        height: '40px'
+      }}
+    >
+    </Box>
+
+    <List>
+      <ListItem>
+        <ListItemButton component='a' href='/dashboard'>
+          <ListItemIcon>
+            <StarIcon />
+          </ListItemIcon>
+          <ListItemText primary='Dashboard' />
+        </ListItemButton>
+      </ListItem>
+
+      <ListItem>
+        <ListItemButton component='a' href='/access-manager'>
+          <ListItemIcon>
+            <StarIcon />
+          </ListItemIcon>
+          <ListItemText primary='Permissions' />
+        </ListItemButton>
+      </ListItem>
+
+      <ListItem>
+        <ListItemButton onClick={logout}>
+          <ListItemIcon>
+            <StarIcon />
+          </ListItemIcon>
+          <ListItemText primary='Logout' />
+        </ListItemButton>
+      </ListItem>
+    </List>
+
+    </Drawer>
+  );
 }
