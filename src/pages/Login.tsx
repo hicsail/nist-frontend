95560d8cd17a9c5d5c739238fa9ebcf69bebf844
Asad <am5815@bu.edu>
2023-05-04 15:54:45 -0400

    removed hardcoded url

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -7,7 +7,7 @@ export default function Login() {
     const navigate = useNavigate();
     const { isAuthenticated } = useContext(AuthContext);
     const projectId = import.meta.env.VITE_PROJECT_ID;
-    const loginUrl = `https://test-auth.sail.codes?projectId=${projectId}&redirectUrl=${encodeURIComponent(window.location.origin + '/auth/callback')}`;
+    const loginUrl = `${import.meta.env.VITE_AUTHSERVICE_URL}?projectId=${projectId}&redirectUrl=${encodeURIComponent(window.location.origin + '/auth/callback')}`;
     useEffect(() => {
         if (isAuthenticated) navigate('/dashboard');
         //`https://test-auth.sail.codes/?projectId=${projectId}`;
