5ef845b3171505ffed02472760c3d94a30cf541a
cbolles <collinbolles@gmail.com>
2023-05-04 19:31:47 -0400

    Remove un-used code, will refer back to it once functionality is re-visited

--- src/pages/Organization.tsx
+++ src/pages/Organization.tsx
@@ -1,29 +1,17 @@
-import React, { useEffect, useState, useContext, ReactNode, FC } from 'react'
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
-import { createFolder, getOrganizationContents } from '../aws-client';
-import FileUploader from '../components/FileUploader';
+import { useEffect, useState, useContext, FC } from 'react'
+import { useNavigate, useParams } from 'react-router-dom';
+import { getOrganizationContents } from '../aws-client';
 import {
   Button,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogActions,
   TextField,
-  CircularProgress,
-  Snackbar,
   Breadcrumbs,
-  Link,
   Typography,
   Box,
   Grid,
   Divider
 } from '@mui/material';
-import { Alert } from '@mui/material';
-import S3Display from '../components/S3Display';
 import { PermissionsContext } from "../contexts/Permissions";
-import Chip from '@mui/material/Chip';
 import { S3Context } from '../contexts/s3.context';
-import { UIContext } from '../contexts/UI';
 import { CargoPermissions } from '../graphql/graphql';
 import HomeIcon from '@mui/icons-material/Home';
 import FolderIcon from '@mui/icons-material/Folder';
@@ -67,13 +55,8 @@ const FileBreadcrumbs: FC<{ path: string}> = ({ path }) => {
 
 export const Organization: FC = () => {
   const { organization } = useContext(OrganizationContext);
-  const [userPermissions, setUserPermissions] = useState<any>();
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [folderName, setFolderName] = useState('');
-  const [isCreatingFolder, setIsCreatingFolder] = useState(false);
-  const [hasCreatedFolder, setHasCreatedFolder] = useState(false);
-  const [files, setFiles] = useState<any[]>([]);
-  const [error, setError] = useState(null);
+  const [setUserPermissions] = useState<any>();
+  const [_, setFiles] = useState<any[]>([]);
   const s3Client = useContext(S3Context);
 
   // Determine the file path to visualize
@@ -84,28 +67,11 @@ export const Organization: FC = () => {
     setPath(`/${splat}`);
   }, [splat]);
 
-  const handleFolderNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setFolderName(event.target.value);
-  };
-
   async function fetchS3Contents() {
     const contents = await getOrganizationContents(s3Client, organization!.bucket);
     setFiles(contents);
   }
 
-  const handleCreateFolder = async () => {
-    setIsCreatingFolder(true);
-    try {
-      await createFolder(s3Client, organization!.bucket, folderName);
-      setHasCreatedFolder(true);
-      fetchS3Contents();
-    } catch (error: any) {
-      setError(error);
-    } finally {
-      setIsCreatingFolder(false);
-      setIsModalOpen(false);
-    }
-  };
   const permissions = useContext(PermissionsContext);
 
   const getPermissionsForOrganization = (bucket: string) => {
@@ -121,9 +87,7 @@ export const Organization: FC = () => {
   }, [permissions]);
 
   useEffect(() => {
-
     fetchS3Contents();
-
   }, [organization]);
 
   return (
@@ -155,73 +119,4 @@ export const Organization: FC = () => {
       <FileListView path={path} bucket={organization?.bucket || null}/>
     </Box>
   );
-
-  /*
-  return (
-    <div>
-      <Breadcrumbs separator="›" aria-label="breadcrumb">
-        {
-          path.map((path: any) => {
-            return (
-              <Link key={path.name} color="text.primary" href={path.path}>
-                {path.name}
-              </Link>
-            )
-          })
-        }
-      </Breadcrumbs>
-      <Typography variant='h2'>{organization.name}</Typography>
-      <div style={{ padding: 10, margin: 10 }}>
-        {
-          (userPermissions && userPermissions.admin) ? <Chip label="Admin" color="success" /> : null
-        }
-        {
-          (userPermissions && userPermissions.write && !userPermissions.admin) ? <Chip label="Write" color="success" /> : null
-        }{
-          (userPermissions && userPermissions.read && !userPermissions.admin) ? <Chip label="Read" color="success" /> : null
-        }{
-          (userPermissions && userPermissions.delete && !userPermissions.admin) ? <Chip label="Delete" color="success" /> : null
-        }
-      </div>
-      {
-        userPermissions && (userPermissions.admin || userPermissions.write) ? (
-          <Button variant="contained" startIcon={<i className="fa fa-folder-plus" aria-hidden="true"></i>} onClick={() => setIsModalOpen(true)}>
-            Create Folder
-          </Button>
-        ) : null
-      }
-      <Dialog open={isModalOpen} onClose={() => setIsModalOpen(false)}>
-        <DialogTitle>Create New Folder</DialogTitle>
-        <DialogContent>
-          <TextField label="Folder Name" value={folderName} onChange={handleFolderNameChange} fullWidth />
-        </DialogContent>
-        <DialogActions>
-          <Button disabled={!folderName || isCreatingFolder} onClick={handleCreateFolder}>
-            {isCreatingFolder ? <CircularProgress size={24} /> : 'Create'}
-          </Button>
-        </DialogActions>
-      </Dialog>
-      <Snackbar open={!!error} autoHideDuration={6000} onClose={() => setError(null)}>
-        <Alert severity="error" sx={{ width: '100%' }} onClose={() => setError(null)}>
-          {error}
-        </Alert>
-      </Snackbar>
-      <Snackbar open={hasCreatedFolder} autoHideDuration={6000} onClose={() => setHasCreatedFolder(false)}>
-        <Alert severity="success" sx={{ width: '100%' }} onClose={() => setHasCreatedFolder(false)}>
-          Folder created successfully!
-        </Alert>
-      </Snackbar>
-      {
-        userPermissions && (userPermissions.admin || userPermissions.write) ? (
-          <FileUploader s3BucketName={location.state.bucket} loadFiles={fetchS3Contents} />
-        ) : null
-      }
-      {
-        userPermissions && (userPermissions.admin || userPermissions.read) ? (
-          <S3Display s3BucketName={location.state.bucket} files={files} loadFiles={fetchS3Contents} />
-        ) : null
-      }
-    </div>
-  )
-  */
 }
