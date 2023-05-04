07929130fc2742b1c7da9a1c2b3d73d30d0719ac
cbolles <collinbolles@gmail.com>
2023-05-04 15:00:01 -0400

    Working ability to view files as list

--- src/components/file-list-view.tsx
+++ src/components/file-list-view.tsx
@@ -3,20 +3,85 @@ import {
   TableContainer,
   TableHead,
   TableCell,
-  TableRow
+  TableRow,
+  Table
 } from '@mui/material';
-import { FC } from 'react';
+import { FC, useContext, useState, useEffect } from 'react';
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import DeleteIcon from '@mui/icons-material/Delete';
+import FolderIcon from '@mui/icons-material/Folder';
+import { S3Context } from '../contexts/s3.context';
+import { ListObjectsCommand, PutObjectCommand, S3Client, _Object as S3Object } from '@aws-sdk/client-s3';
+import { useLocation } from 'react-router-dom';
+
+// TODO: Handle when there are more then 1000 objects
+const getObjectsForPath = async (s3Client: S3Client, bucket: string, path: string): Promise<S3Object[]> => {
+  // Get the files that match the given prefix
+  const result = await s3Client.send(new ListObjectsCommand({ Bucket: bucket, Prefix: path }));
+  if (!result.Contents) { return []; }
+  let objects = result.Contents;
+
+  // Filter out files that are not in the current "folder"
+  return objects.filter((object) => {
+    const key = object.Key;
+    if (!key) {
+      return false;
+    }
+
+    // Trim off the path from the begining of the key, then check to see
+    // if the object represents a folder or a file at the top level
+    const pathRemainder = key.substring(path.length).split('/');
+    return pathRemainder.length == 1 || (pathRemainder.length == 2 && pathRemainder[1] == '');
+  });
+}
+
+interface FileRowProps {
+  object: S3Object;
+}
+
+const FileRowView: FC<FileRowProps> = ({ object }) => {
+  const fileComponents = object.Key!.split('/');
+  const isFolder = fileComponents[fileComponents.length - 1] == '';
+
+  // Get the name which is the last element in the path split on '/' or the
+  // second to last in the case of a folder
+  const name = fileComponents[isFolder ? fileComponents.length - 2 : fileComponents.length - 1];
+
+  return (
+    <TableRow>
+      <TableCell>{isFolder ? <FolderIcon /> : <></>}{name}</TableCell>
+      <TableCell>{object.LastModified ? object.LastModified.getDate() : ''}</TableCell>
+      <TableCell>{object.Size}</TableCell>
+    </TableRow>
+  );
+};
 
 export const FileListView: FC = () => {
+  const s3Client = useContext(S3Context);
+  const location = useLocation();
+  const organization = location.state;
+  const [path, setPath] = useState('/');
+  const [objects, setObjects] = useState<S3Object[]>([]);
+
+  useEffect(() => {
+    getObjectsForPath(s3Client, organization.bucket, path).then((objs) => setObjects(objs));
+  }, [path]);
+
   return (
-    <TableContainer component={Paper}>
-      <TableHead>
-        <TableRow>
-          <TableCell>Name</TableCell>
-          <TableCell>Created Date</TableCell>
-          <TableCell>Size</TableCell>
-        </TableRow>
-      </TableHead>
+    <TableContainer component={Paper} sx={{ minWidth: 650 }}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell>Created Date</TableCell>
+            <TableCell>Size</TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+        </TableHead>
+
+        {objects.map((object) => <FileRowView object={object} />)}
+
+      </Table>
     </TableContainer>
   );
 }
