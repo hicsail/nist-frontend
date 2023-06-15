6d735f35bee6b96294b12316d6183e53e19126c4
cbolles <collinbolles@gmail.com>
2023-06-15 11:41:31 -0400

    Working ability to navigate into folder via url

--- src/pages/Organization.tsx
+++ src/pages/Organization.tsx
@@ -4,6 +4,7 @@ import { S3Context } from '../contexts/s3.context';
 import { CargoPermissions } from '../graphql/graphql';
 import { OrganizationContext } from '../contexts/organization.context';
 import { S3Viewer, DocViewPlugin } from '@bu-sail/s3-viewer';
+import { useParams } from 'react-router-dom';
 
 export const Organization: FC = () => {
   const { organization } = useContext(OrganizationContext);
@@ -17,12 +18,23 @@ export const Organization: FC = () => {
     return orgPermissions;
   };
 
+  // Handle getting the user permissions for the organization
   useEffect(() => {
     if (permissions && organization) {
       const permissions = getPermissionsForOrganization(organization.bucket);
       setUserPermissions(permissions || null);
     }
   }, [permissions]);
+
+  // Provides controls for the path based on the user navigation
+  const [currentPath, setCurrentPath] = useState<string>('');
+
+  // Base the path on the splat
+  const splat = useParams()['*'];
+
+  useEffect(() => {
+    setCurrentPath(`${splat}`);
+  }, [splat]);
 
   // TODO:
   //  * Allow navigation changes (going back/forward) to change the path of
@@ -34,6 +46,7 @@ export const Organization: FC = () => {
         bucket={organization!.bucket}
         bucketDisplayedName={organization!.name}
         client={s3Client}
+        pathControl={{ currentPath, setCurrentPath }}
         plugins={[new DocViewPlugin()]}
         disableRead={!userPermissions.read}
         disableWrite={!userPermissions.write}
