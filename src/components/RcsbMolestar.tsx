1e61974f3487d824d8670e7d744213b8c928e9b5
cbolles <collinbolles@gmail.com>
2023-06-21 13:31:07 -0400

    Fix iframe size

--- src/components/RcsbMolestar.tsx
+++ src/components/RcsbMolestar.tsx
@@ -1,7 +1,22 @@
-import { FC, LegacyRef, ReactNode, useEffect, useRef, useState } from 'react';
+import { FC, ReactNode, useEffect, useRef, useState } from 'react';
 import { Plugin, S3Object, useS3Context } from '@bu-sail/s3-viewer';
 import { Viewer } from '@rcsb/rcsb-molstar/build/src/viewer/index';
 import { createPortal } from 'react-dom';
+import { Paper, Typography, Divider } from '@mui/material';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: '90%',
+  height: '90%',
+  bgcolor: 'background.paper',
+  overflow: 'hidden',
+  display: 'flex',
+  flexDirection: 'column',
+  p: 4
+};
 
 export class RcsbMolestarPlugin implements Plugin {
   name: string;
@@ -20,6 +35,7 @@ export class RcsbMolestarPlugin implements Plugin {
 }
 
 const MolestarWrapper: FC<{ object: S3Object }> = ({ object }) => {
+  // Handle the logic to embed the view in an iframe
   const node = useRef<HTMLIFrameElement>(null);
   const [doc, setDoc] = useState<Document | null>();
 
@@ -36,14 +52,24 @@ const MolestarWrapper: FC<{ object: S3Object }> = ({ object }) => {
   };
 
   useEffect(() => {
-    console.log('here');
     loadPDB();
   }, []);
 
+  // Wrap the protein view in an iframe since the view is not provided
+  // as a typical React node.
   return (
-    <iframe ref={node}>
-      {node && doc && url && createPortal(<Molestar url={url}/>, doc.body)}
-    </iframe>
+    <Paper sx={style}>
+      <Typography variant="h6" sx={{ m: 0, p: 2 }}>
+        {'RCSB Molestar'}
+      </Typography>
+      <Divider sx={{ my: 2 }} />
+      <div style={{ flexGrow: 1 }}>
+        <iframe ref={node} style={{ width: '100%', height: '100%' }}>
+          {node && doc && url && createPortal(<Molestar url={url}/>, doc.body)}
+        </iframe>
+      </div>
+  </Paper>
+
   );
 };
 
@@ -51,16 +77,18 @@ const Molestar: FC<{ url: string }> = ({ url }) => {
   const ref = useRef(null);
 
   useEffect(() => {
-    if (!ref.current) { return; }
+    if (!ref.current) {
+      return;
+    }
 
     const viewer = new Viewer(ref.current, {
       showImportControls: true,
       showSessionControls: true,
-      layoutShowLog: true,
+      layoutShowLog: false,
       layoutShowControls: true,
       showMembraneOrientationPreset: true,
       showNakbColorTheme: true,
-      detachedFromSierra: true, // needed when running without sierra
+      detachedFromSierra: true
     });
 
     viewer.loadStructureFromUrl(url, 'pdb', false);
