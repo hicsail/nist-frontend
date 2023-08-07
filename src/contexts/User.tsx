9918183bd7853508121d32d20e8638f27cb48042
Asad <am5815@bu.edu>
2023-08-07 15:15:58 -0400

    added frontend handling of updates

--- src/contexts/User.tsx
+++ src/contexts/User.tsx
@@ -1,17 +1,44 @@
 import { createContext, ReactNode, FC, useEffect, useState } from 'react';
 import { useGetMeQuery, useUpdateUserMutation } from '../graphql/user/user';
 import { UserModel } from '../graphql/graphql';
+import React from 'react';
 
-export const UserContext = createContext<UserModel | undefined>(undefined);
 
 interface UserProviderProps {
   children: ReactNode;
 }
 
+interface UserContextProps {
+  user: UserModel | undefined;
+  setUser: React.Dispatch<React.SetStateAction<UserModel | undefined>>;
+  updateError: boolean;
+  updateSuccess: boolean;
+  updateErrorMessage: string;
+  setUpdateError: React.Dispatch<React.SetStateAction<boolean>>;
+  setUpdateSuccess: React.Dispatch<React.SetStateAction<boolean>>;
+  setUpdateErrorMessage: React.Dispatch<React.SetStateAction<string>>;
+  updating: boolean;
+}
+
+export const UserContext = createContext<UserContextProps>({
+   user: undefined, 
+   setUser: () => { }, 
+   updateError: false,
+   updateSuccess: false,
+   updateErrorMessage: '',
+   setUpdateError: () => { },
+   setUpdateSuccess: () => { },
+   setUpdateErrorMessage: () => { },
+   updating: false,
+});
+
 export const UserProvider: FC<UserProviderProps> = ({ children }) => {
   const [user, setUser] = useState<UserModel | undefined>();
   const { data } = useGetMeQuery({});
-  const [updateUser] = useUpdateUserMutation({});
+  const [updateUser, {loading : updating}] = useUpdateUserMutation({});
+  const [updateSuccess, setUpdateSuccess] = useState<boolean>(false);
+  const [updateError, setUpdateError] = useState<boolean>(false);
+  const [updateErrorMessage, setUpdateErrorMessage] = useState<string>('');
 
   useEffect(() => {
     if (data?.me) {
@@ -20,15 +47,47 @@ export const UserProvider: FC<UserProviderProps> = ({ children }) => {
   }, [data]);
 
   useEffect(() => {
-    if (user && user.fullname && user.email) {
+    if (user && user.fullname && user.email && (data?.me.fullname !== user.fullname || data?.me.email !== user.email)) {
       updateUser({
         variables: {
           fullname: user.fullname,
           email: user.email,
+        },
+        onCompleted: () => {
+          setUpdateSuccess(true);
+        },
+        onError: (error) => {
+          setUpdateError(true);
+          setUpdateErrorMessage(error.message);
+          setUser({
+            ...user,
+            fullname: data?.me?.fullname,
+            email: data?.me?.email,
+          })
         }
       });
     }
   }, [user]);
 
-  return <UserContext.Provider value={user}>{children}</UserContext.Provider>;
+  const value = {
+    user,
+    setUser,
+    updateError,
+    updateSuccess,
+    updateErrorMessage,
+    setUpdateError,
+    setUpdateSuccess,
+    setUpdateErrorMessage,
+    updating,
+  };
+
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
+
+export const useUser = () => {
+  const context = React.useContext(UserContext);
+  if (context === undefined) {
+    throw new Error('useUser must be used within a UserProvider');
+  }
+  return context;
+}
--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,7 +1,92 @@
-import React from 'react'
+import { Button, Card, FormControl, Input, Paper, Snackbar, TextField, Typography } from '@mui/material';
+import React from 'react';
+import { useUser } from '../contexts/User';
 
 export default function Account() {
+  const { user, setUser, updateError, setUpdateError, updateSuccess, setUpdateSuccess, updateErrorMessage, setUpdateErrorMessage, updating } = useUser();
+  const [fullname, setFullname] = React.useState<any>();
+  const [email, setEmail] = React.useState<any>();
+  const [snackbarOpen, setSnackbarOpen] = React.useState<boolean>(false);
+  const [snackbarMessage, setSnackbarMessage] = React.useState<string>('');
+
+  React.useEffect(() => {
+    if (user) {
+      setFullname(user?.fullname);
+      setEmail(user?.email);
+    }
+  }, [user]);
+
+  React.useEffect(() => {
+    if (updateError) {
+      setSnackbarOpen(true);
+      setUpdateError(false);
+      setSnackbarMessage(updateErrorMessage);
+      handleRevert();
+    }
+  }, [updateError]);
+
+  React.useEffect(() => {
+    if (updateSuccess) {
+      setSnackbarOpen(true);
+      setUpdateSuccess(false);
+      setSnackbarMessage('Successfully updated user');
+    }
+  }, [updateSuccess]);
+
+  const handleSave = () => {
+    // check if fullname and email are valid
+    if (!user) return;
+    if (!fullname || !email) return;
+    if (fullname.length < 3 || email.length < 3) return;
+    if (!email.includes('@')) return;
+
+    setUser({
+      ...user,
+      fullname,
+      email
+    });
+  };
+
+  const handleRevert = () => {
+    if (user) {
+      setFullname(user.fullname);
+      setEmail(user.email);
+    }
+  };
+
+  const handleSnackbarClose = () => {
+    setSnackbarOpen(false);
+    setUpdateSuccess(false);
+    setUpdateError(false);
+    setUpdateErrorMessage('');
+    setSnackbarMessage('');
+  };
+
   return (
-    <div>Account</div>
-  )
+    <div>
+      <Typography variant="h1">Account</Typography>
+      <Paper
+        variant="outlined"
+        sx={{
+          padding: '2rem',
+          marginTop: '2rem'
+        }}
+      >
+        <FormControl fullWidth sx={{
+          maxWidth: '500px'
+        }}>
+          <TextField label="Full Name" value={fullname ? fullname : ''} onChange={(e) => setFullname(e.target.value)} placeholder="Full Name" margin="normal"/>
+          <TextField label="Email" value={email ? email : ''} onChange={(e) => setEmail(e.target.value)} placeholder="Email" margin="normal"/>
+          <div>
+            <Button onClick={handleSave} disabled={updating}>
+              Save
+            </Button>
+            <Button onClick={handleRevert}>Revert</Button>
+          </div>
+          <div>{updating && <Typography variant="caption">Updating...</Typography>}</div>
+        </FormControl>
+      </Paper>
+      <Snackbar open={snackbarOpen} autoHideDuration={6000} onClose={handleSnackbarClose} message={snackbarMessage} />
+    </div>
+  );
 }
