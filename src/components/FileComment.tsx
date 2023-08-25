c30c3b32055abd45b8a64cef72abd67ff34a043c
wenhwang97 <wenhwang@bu.edu>
2023-08-25 17:09:52 -0400

    separate reply from comment

--- src/components/FileComment.tsx
+++ src/components/FileComment.tsx
@@ -13,6 +13,10 @@ import {
   Divider,
   Grid,
   IconButton,
+  ListItemIcon,
+  ListItemText,
+  Menu,
+  MenuItem,
   Stack,
   TextField,
   Typography
@@ -21,9 +25,11 @@ import AddCommentIcon from '@mui/icons-material/AddComment';
 import CommentIcon from '@mui/icons-material/Comment';
 import SendIcon from '@mui/icons-material/Send';
 import ReplyIcon from '@mui/icons-material/Reply';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import DeleteIcon from '@mui/icons-material/Delete';
 import { OrganizationContext } from '../contexts/organization.context';
 import { DeleteFileDocument, useAddFileMutation, useGetFileQuery } from '../graphql/file/file';
-import { useAddCommentMutation, useAddReplyMutation, useGetCommentsQuery } from '../graphql/comment/comment';
+import { useAddCommentMutation, useAddReplyMutation, useDeleteCommentMutation, useGetCommentsQuery } from '../graphql/comment/comment';
 import { ApolloClient } from '@apollo/client';
 
 export type Comment = {
@@ -76,6 +82,147 @@ export class FileCommentPlugin implements SideNavPlugin {
   }
 }
 
+interface CommentPanelProps {
+  object: S3Object | undefined;
+  comment: Comment;
+  expand: boolean;
+  onDelete: (reply: Comment) => void;
+}
+
+const ReplyPanel: FC<CommentPanelProps> = (props) => {
+  const { object, comment, expand, onDelete: handleDeleteCommentReply } = props;
+
+  const commentQuery = useGetCommentsQuery({
+    variables: {
+      fileId: object?.id!
+    }
+  });
+  const [addReplyMutation] = useAddReplyMutation();
+
+  const [expandReply, setExpandReply] = useState<boolean>(false);
+  const [replyTo, setReplyTo] = useState<User | null>(null);
+  const [replyContents, setReplyContents] = useState<string>('');
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleClickMore = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleCloseMore = () => {
+    setAnchorEl(null);
+  };
+
+  const handleExpandReply = (replyTo: User | null) => {
+    setReplyTo(replyTo);
+    setExpandReply(true);
+  };
+
+  const handleCollapseReply = () => {
+    setReplyTo(null);
+    setExpandReply(false);
+  };
+
+  const handleReplyInput = (event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+    setReplyContents(event.target.value);
+  };
+
+  const handleReply = async (_user: User | null, content: string, commentId: string) => {
+    // TODO: may need to fix comment schema to include replyTo field
+    // api call to post comment
+    await addReplyMutation({
+      variables: {
+        fileId: object?.id!,
+        parentId: commentId,
+        content
+      }
+    });
+
+    commentQuery.refetch();
+
+    setReplyTo(null);
+    setReplyContents('');
+    setExpandReply(false);
+  };
+
+  useEffect(() => {
+    if (!expand) setExpandReply(expand);
+  }, [expand]);
+
+  return (
+    <>
+      <Collapse in={expand} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              width: '100%'
+            }}
+          >
+            <Divider orientation="vertical" variant="middle" flexItem sx={{ border: 1, borderColor: '#E0E0E0', marginRight: '16px' }} />
+            <Box width="100%">
+              {comment.replies?.map((reply: any, replyIndex: number) => (
+                <Stack key={reply._id} spacing="8px">
+                  {replyIndex !== 0 && <Divider sx={{ paddingTop: '8px' }} />}
+                  <Box display="flex">
+                    <Box marginRight="16px">
+                      <Avatar>{(reply.user.fullname || reply.user.username || reply.user.email)[0]}</Avatar>
+                    </Box>
+                    <Box>
+                      <Typography variant="body2">{reply.user.fullname || reply.user.username || reply.user.email}</Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        {formatDate(new Date(reply.date))}
+                      </Typography>
+                    </Box>
+                  </Box>
+                  <Box>
+                    <Typography variant="body2">{reply._id}</Typography>
+                    <Typography variant="body2">{reply.content}</Typography>
+                  </Box>
+                  <Box textAlign="end">
+                    <IconButton size="small" onClick={() => handleExpandReply(reply.user)}>
+                      <ReplyIcon fontSize="small" sx={{ margin: 0 }} />
+                    </IconButton>
+                    <IconButton size="small" onClick={handleClickMore}>
+                      <MoreHorizIcon fontSize="small" sx={{ margin: 0 }} />
+                    </IconButton>
+                    <Menu anchorEl={anchorEl} open={open} onClose={handleCloseMore} elevation={2}>
+                      <MenuItem onClick={() => handleDeleteCommentReply(reply)}>
+                        <ListItemIcon>
+                          <DeleteIcon fontSize="small" />
+                        </ListItemIcon>
+                        <ListItemText>Delete</ListItemText>
+                      </MenuItem>
+                    </Menu>
+                  </Box>
+                </Stack>
+              ))}
+            </Box>
+          </Box>
+        </CardContent>
+      </Collapse>
+      <Collapse in={expandReply} timeout="auto" unmountOnExit>
+        <ClickAwayListener onClickAway={handleCollapseReply}>
+          <CardContent sx={{ paddingY: '8px' }}>
+            <TextField
+              placeholder={replyTo ? `Reply to ${replyTo.fullname || replyTo.username || replyTo.email}:` : ''}
+              variant="standard"
+              sx={{ width: 'calc(100% - 40px)' }}
+              value={replyContents}
+              onChange={handleReplyInput}
+              multiline
+            />
+            <IconButton onClick={() => handleReply(replyTo, replyContents, comment._id)}>
+              <SendIcon />
+            </IconButton>
+          </CardContent>
+        </ClickAwayListener>
+      </Collapse>
+    </>
+  );
+};
+
 const FileCommentPanel: FC<{ object: S3Object | undefined }> = ({ object }) => {
   const { organization } = useContext(OrganizationContext);
 
@@ -105,15 +252,24 @@ const FileCommentPanel: FC<{ object: S3Object | undefined }> = ({ object }) => {
 
   const [expandThread, setExpandThread] = useState<{ [key: number]: boolean }>(initExpandThreadState);
   const [expandReply, setExpandReply] = useState<{ [key: number]: boolean }>(initExpandReplyState);
-  const [replyTo, setReplyTo] = useState<User | null>(null);
   const [replyContents, setReplyContents] = useState<{ [key: number]: string }>({});
   const [newComment, setNewComment] = useState<string>('');
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleClickMore = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleCloseMore = () => {
+    setAnchorEl(null);
+  };
 
   const handleExpandThread = (index: number) => {
     setExpandThread((prev) => ({ ...prev, [index]: !prev[index] }));
   };
 
-  const handleExpandReply = (replyTo: User | null, index: number) => {
+  const handleExpandReply = (index: number) => {
     const newState = comments.reduce((acc, _, index) => {
       acc[index] = false;
       return acc;
@@ -121,12 +277,10 @@ const FileCommentPanel: FC<{ object: S3Object | undefined }> = ({ object }) => {
 
     newState[index] = true;
 
-    setReplyTo(replyTo);
     setExpandReply(newState);
   };
 
   const handleCollapseReply = (index: number) => {
-    setReplyTo(null);
     setExpandReply((prev) => ({ ...prev, [index]: false }));
   };
 
@@ -143,7 +297,7 @@ const FileCommentPanel: FC<{ object: S3Object | undefined }> = ({ object }) => {
     setNewComment('');
   };
 
-  const handleReply = async (_user: User | null, content: string, index: number, commentId: string) => {
+  const handleReply = async (content: string, index: number, commentId: string) => {
     // TODO: may need to fix comment schema to include replyTo field
     // api call to post comment
     await addReplyMutation({
@@ -156,7 +310,6 @@ const FileCommentPanel: FC<{ object: S3Object | undefined }> = ({ object }) => {
 
     commentQuery.refetch();
 
-    setReplyTo(null);
     setReplyContents((prev) => ({ ...prev, [index]: '' }));
     setExpandReply((prev) => ({ ...prev, [index]: false }));
   };
@@ -167,6 +320,17 @@ const FileCommentPanel: FC<{ object: S3Object | undefined }> = ({ object }) => {
 
   const handleCommentInput = (event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
     setNewComment(event.target.value);
+  };
+
+  const handleDeleteCommentReply = (comment: Comment) => {
+    // await deleteCommentMutation({
+    //   variables: {
+    //     id
+    //   }
+    // });
+    // commentQuery.refetch();
+    console.log(comment);
+    handleCloseMore();
   };
 
   useEffect(() => {
@@ -204,11 +368,12 @@ const FileCommentPanel: FC<{ object: S3Object | undefined }> = ({ object }) => {
                 sx={{ paddingBottom: '8px' }}
               />
               <CardContent sx={{ paddingY: '8px' }}>
+                <Typography variant="body2">{comment._id}</Typography>
                 <Typography variant="body2">{comment.content}</Typography>
               </CardContent>
               <CardActions disableSpacing>
                 <Box marginLeft="auto">
-                  <IconButton onClick={() => handleExpandReply(null, index)}>
+                  <IconButton onClick={() => handleExpandReply(index)}>
                     <AddCommentIcon />
                   </IconButton>
                   <IconButton disabled={!Boolean(comment.replies) || comment.replies?.length === 0} onClick={() => handleExpandThread(index)}>
@@ -216,59 +381,25 @@ const FileCommentPanel: FC<{ object: S3Object | undefined }> = ({ object }) => {
                       <CommentIcon />
                     </Badge>
                   </IconButton>
+                  <IconButton onClick={handleClickMore}>
+                    <MoreHorizIcon />
+                  </IconButton>
+                  <Menu anchorEl={anchorEl} open={open} onClose={handleCloseMore} elevation={2}>
+                    <MenuItem onClick={() => handleDeleteCommentReply(comment)}>
+                      <ListItemIcon>
+                        <DeleteIcon fontSize="small" />
+                      </ListItemIcon>
+                      <ListItemText>Delete</ListItemText>
+                    </MenuItem>
+                  </Menu>
                 </Box>
               </CardActions>
-              <Collapse in={expandThread[index]} timeout="auto" unmountOnExit>
-                <CardContent>
-                  <Box
-                    sx={{
-                      display: 'flex',
-                      alignItems: 'center',
-                      width: '100%'
-                    }}
-                  >
-                    <Divider orientation="vertical" variant="middle" flexItem sx={{ border: 1, borderColor: '#E0E0E0', marginRight: '16px' }} />
-                    <Box width="100%">
-                      {comment.replies?.map((reply: any, replyIndex: number) => (
-                        <Stack key={reply._id} spacing="8px">
-                          {replyIndex !== 0 && <Divider sx={{ paddingTop: '8px' }} />}
-                          <Box display="flex">
-                            <Box marginRight="16px">
-                              <Avatar>{(comment.user.fullname || comment.user.username || comment.user.email)[0]}</Avatar>
-                            </Box>
-                            <Box>
-                              <Typography variant="body2">{comment.user.fullname || comment.user.username || comment.user.email}</Typography>
-                              <Typography variant="body2" color="text.secondary">
-                                {formatDate(new Date(reply.date))}
-                              </Typography>
-                            </Box>
-                          </Box>
-                          <Box>
-                            <Typography variant="body2">{reply.content}</Typography>
-                          </Box>
-                          <Box textAlign="end">
-                            <IconButton size="small" onClick={() => handleExpandReply(reply.user, index)}>
-                              <ReplyIcon fontSize="small" sx={{ margin: 0 }} />
-                            </IconButton>
-                          </Box>
-                        </Stack>
-                      ))}
-                    </Box>
-                  </Box>
-                </CardContent>
-              </Collapse>
+              <ReplyPanel object={object} comment={comment} expand={expandThread[index]} onDelete={handleDeleteCommentReply} />
               <Collapse in={expandReply[index]} timeout="auto" unmountOnExit>
                 <ClickAwayListener onClickAway={() => handleCollapseReply(index)}>
                   <CardContent sx={{ paddingY: '8px' }}>
-                    <TextField
-                      placeholder={replyTo ? `Reply to ${replyTo.fullname || replyTo.username || replyTo.email}:` : ''}
-                      variant="standard"
-                      sx={{ width: 'calc(100% - 40px)' }}
-                      value={replyContents[index]}
-                      onChange={(event) => handleReplyInput(event, index)}
-                      multiline
-                    />
-                    <IconButton onClick={() => handleReply(replyTo, replyContents[index], index, comment._id)}>
+                    <TextField variant="standard" sx={{ width: 'calc(100% - 40px)' }} value={replyContents[index]} onChange={(event) => handleReplyInput(event, index)} multiline />
+                    <IconButton onClick={() => handleReply(replyContents[index], index, comment._id)}>
                       <SendIcon />
                     </IconButton>
                   </CardContent>
